af0a32bb5a2c8a973d9b46cd8cbcb4c6d028c4b9
Mohameme7 <donttakemeplease@gmail.com>
2024-10-21 23:27:54 +0400

    Added GRIB Merger

---
+++ MetPlot/Downloader/FileHandler.py
@@ -0,0 +1,30 @@
+import io
+import os
+import tempfile
+
+import pygrib
+from MetPlot.Downloader.MemoryDownload import Downloader
+
+
+class GribCreation:
+    def __init__(self, Data, FileName: str):
+        self.Data = Data
+        self.FileName = FileName
+        self.merge_grib_files()
+
+    def merge_grib_files(self) -> None:
+        """Merge multiple GRIB data into one file."""
+        with open(self.FileName, 'wb') as outfile:
+            for i, grib_content in enumerate(self.Data):
+
+                    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                        temp_file.write(grib_content)
+                        temp_file.flush()
+
+                        with pygrib.open(temp_file.name) as gf:
+                            for msg in gf:
+                                outfile.write(msg.tostring())
+                        temp_file.close()
+                        os.remove(temp_file.name)
+
+
