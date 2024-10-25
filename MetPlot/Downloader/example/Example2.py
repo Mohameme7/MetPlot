7a1404cafe5953d0ad956d9c0c7c652502fbd340
Mohameme7 <donttakemeplease@gmail.com>
2024-10-26 00:08:22 +0400

    Updated Example, Now has Interpolation as well

--- MetPlot/Downloader/example/Example2.py
+++ MetPlot/Downloader/example/Example2.py
@@ -1,9 +1,13 @@
-from MetPlot.Downloader.example.FinaD import download
+from MetPlot.Downloader.example.FinaD import download; from MetPlot.Interpolation import GribInterpolator
 import time
 
 start_time = time.time()
 
-download(list(range(1, 91)), '2024-10-22', '06', ['APCP', 'SPFH'],
-         ['surface', 1000, 925, 850], '../Forecast.grib', [42, 12, 25, 60])
+download(list(range(1, 121)) + list(range(120,385,3)), '2024-10-24', '12', ['SPFH'],
+         [1000], 'Forecast.grib', [42, 12, 25, 60])
+
+with GribInterpolator('Forecast.grib', 'InterpolatedForecast.grib') as inter:
+    interpolated_values = inter.run_interpolation()
+    inter.merge_to_grib(interpolated_values)
 
 print(f"{time.time() - start_time} seconds")
