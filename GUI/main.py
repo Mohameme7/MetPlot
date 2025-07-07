f899fa9a987ab708273447283c5f9c8051af0bd3
Mohameme7 <donttakemeplease@gmail.com>
2025-07-07 23:44:04 +0400

    GUI Release Alongside with 2 model data

---
+++ GUI/main.py
@@ -0,0 +1,192 @@
+import json
+import os
+import logging
+import subprocess
+
+import requests
+import webview
+from nicegui import ui, app
+from GUI.modeldownloads import GFS_Load
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+app.title = 'MetPlot'
+CoordsHTML = open('templates/CoordsMAP.html').read()
+content = requests.get('https://nomads.ncep.noaa.gov/gribfilter.php?ds=gfs_0p25').content
+top_entry = None
+bottom_entry = None
+left_entry = None
+right_entry = None
+download_button : ui.button
+temp_elements = []
+
+def global_navbar():
+    ui.dark_mode().enable()
+    with ui.row().style('''
+        background-color: #1a1a1d; color: white; display: flex; justify-content: space-between; 
+        align-items: center; padding: 15px 30px; position: fixed; top: 0; left: 0; right: 0; 
+        z-index: 1000; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    '''):
+        ui.label('MetPlot').style('''
+            font-weight: bold; text-transform: uppercase; letter-spacing: 2px;
+            background-color: #ef4444; color: white; font-size: 24px;
+            padding: 20px 48px; border-radius: 12px;
+        ''')
+        with ui.row().style('display: flex; gap: 30px;'):
+            ui.link('Home', '/').style(
+                'color: white; text-decoration: none; font-size: 16px; text-transform: uppercase; transition: color 0.3s ease, transform 0.3s ease;')
+            ui.link('Download', '/download').style(
+                'color: white; text-decoration: none; font-size: 16px; text-transform: uppercase; transition: color 0.3s ease, transform 0.3s ease;')
+            ui.link('Settings', '/settings').style(
+                'color: white; text-decoration: none; font-size: 16px; text-transform: uppercase; transition: color 0.3s ease, transform 0.3s ease;')
+
+
+
+def file_read(file):
+    with open(file) as file:
+        if file.name.endswith('.json'):
+            content = json.load(file)
+        else:
+            content = file.readlines()
+        file.close()
+    return content
+
+@app.get('/update_coordinates/{top}/{bottom}/{left}/{right}')
+def update_coordinates(top: float, bottom: float, left: float, right: float):
+    global top_entry, bottom_entry, left_entry, right_entry
+    top_entry.value = str(top)
+    bottom_entry.value = str(bottom)
+    left_entry.value = str(left)
+    right_entry.value = str(right)
+
+def model_load(variables_file : str, levels_file : str, model : str):
+    while temp_elements:
+        element = temp_elements.pop()
+        element.delete()
+
+    if model =='GFS':
+     GFS_Load(download_button, top_entry, bottom_entry, left_entry, right_entry, temp_elements, content)
+    else:
+        ui.notify("Functionality not implemented for models other than GFS")
+
+
+    variables = file_read(variables_file)
+    level_data = file_read(levels_file)
+    level_data = [(line.strip(), line.strip().replace("lev_", "").replace("_mb", "mb"))
+                  for line in level_data]
+
+    with ui.column().style('margin-top: 15px; padding: 10px;') as el:
+        temp_elements.append(el)
+
+        ui.label('Variables').style('font-size: 14px;')
+        keys = list(variables.keys())
+
+        for i in range(0, len(keys), 12):
+            with ui.row().classes('flex-wrap').style(
+                    'align-items: center; margin-bottom: -8px; justify-content: space-evenly; gap: 5px;'
+            ):
+                for key in keys[i:i + 12]:
+                    levels_str = ', '.join(variables[key]['levels'])
+                    ui.checkbox(text=key).tooltip(f'Name: {variables[key]["name"]}, \n Levels: {levels_str}'
+                                                  ).mark('Variable')
+
+    ###################################################################
+    with ui.column().style('margin-top: 15px; padding: 10px;') as el:
+        temp_elements.append(el)
+
+        chunk_size = max(10, len(level_data) // 20)
+
+        ui.label('Vertical Levels').style('font-size: 14px;')
+
+        for i in range(5, len(level_data), chunk_size):
+            with ui.row().classes('flex-wrap').style('align-items: center; margin-bottom: -8px;'):
+                for original, formatted in level_data[i:i + chunk_size]:
+                    ui.checkbox(text=formatted).mark(f'Level {original}')
+
+
+MODELS = {'GFS': lambda : model_load('static/Variables/GFS/MERGED_PARAMS.json',
+                                     'static/Variables/GFS/VERTICAL_LEVELS.txt', 'GFS'),
+
+          'GEM' : lambda : model_load('static/Variables/GEM/MERGED_PARAMS.json',
+                                      'static/Variables/GEM/VERTICAL_LEVELS.txt', 'GEM')}
+
+
+
+
+
+
+
+
+
+
+@ui.page('/')
+def main():
+    global_navbar()
+
+    with ui.column().style('margin-top: 100px; padding: 20px;'):
+        ui.label('An App to Download Meteorological Data')
+
+
+@ui.page('/download')
+def downloader():
+    global_navbar()
+    ui.select(label='Model', options=[option for option in MODELS],
+              on_change=
+              lambda model: MODELS[model.value]()).style('margin-top:100px;')
+
+    with ui.row().style("gap: 10px; margin-top: 20px; padding: 10px;"):
+        global top_entry, bottom_entry, left_entry, right_entry, download_button
+
+        for label in ['Top Latitude', 'Bottom Latitude', 'Left Longitude', 'Right Longitude']:
+            with ui.column():
+                ui.label(label)
+                entry = ui.input(on_change=lambda: update_bounding_box_from_inputs())
+                globals()[f"{label.split()[0].lower()}_entry"] = entry
+
+        with ui.column():
+          download_button= ui.button("Download").style(
+              'margin-top:50px;'
+          )
+
+
+
+    def update_bounding_box_from_inputs():
+        try:
+            top = float(top_entry.value)
+            bottom = float(bottom_entry.value)
+            left = float(left_entry.value)
+            right = float(right_entry.value)
+
+            ui.run_javascript(f'''
+                updateBoundingBoxFromInputs({top}, {bottom}, {left}, {right});
+            ''')
+
+        except ValueError:
+            ui.notify('Please enter valid coordinates.')
+
+    ui.add_body_html(CoordsHTML)
+
+async def set_panoply_path():
+    file = await app.native.main_window.create_file_dialog(dialog_type=webview.OPEN_DIALOG  )
+    if file:
+     subprocess.run(['setx', 'PANOPLY_PATH', file[0]], shell=True)
+     ui.notify("Set Panoply Path")
+    else:
+        return
+
+
+@ui.page('/settings')
+def settings_page():
+    global_navbar()
+    ui.button(text='Set Panoply Path', on_click=set_panoply_path).style('margin-top:100px;')
+
+    with ui.row().style('margin-top: 15px; padding: 20px;'):
+
+      if os.getenv('PANOPLY_PATH'):
+        ui.label(f"Panoply Path is set : {os.getenv('PANOPLY_PATH')}")
+      else:
+          ui.label("Panoply Path is not set yet.")
+
+
+ui.run(native=True, window_size=(1600, 900), favicon='⛈️', title='MetPlot')
---
+++ GUI/modeldownloads.py
@@ -0,0 +1,115 @@
+import os
+import subprocess
+import threading
+
+import webview
+from nicegui import ui, ElementFilter, app
+
+from MetPlot.Downloader.FileHandler import GribCreation
+from MetPlot.Downloader.MemoryDownload import Downloader
+from MetPlot.Downloader.Parsers.NomadsUtils import NomadsParse
+from MetPlot.Downloader.Parsers.GEM import GEM
+from itertools import product
+
+def GFS_download(**kwargs):
+    hours = kwargs.get('hours')
+    run_date = kwargs.get('run_date')
+    run_time = kwargs.get('run_time')
+    variables = kwargs.get('variables')
+    levels=kwargs.get('levels')
+    filename = kwargs.get('filename')
+    subregion = kwargs.get('subregion')
+
+    urls = []
+    for hour in hours:
+
+     urls.append(NomadsParse.create_url(hour, run_date, run_time, variables, levels, subregion))
+    download_object = Downloader(urls)
+    downloaded_data = download_object.submitdownloads()
+    GribCreation(downloaded_data, filename).merge_grib_files()
+
+def GFS_Load(download_button, top_entry, bottom_entry, left_entry, right_entry, generated_elements, content):
+    nomads = NomadsParse(content)
+
+    async def Download(run_date, run_time):
+        file = await app.native.main_window.create_file_dialog(dialog_type=webview.SAVE_DIALOG)
+
+        information = nomads.get_available_runs()
+        most_recent_run = next(iter(information))
+        if run_date == most_recent_run and run_time == information[most_recent_run][0]:
+            hours = nomads.get_forecast_hours()
+
+        else:
+            hours = list(range(1, 121, 1)) + list(range(123, 385, 3))
+
+        def confirm_selection():
+            if int(to_hour.value) > int(from_hour.value):
+                selected_hours = list(range(int(from_hour.value), int(to_hour.value) + 1))
+                selected_vars = [cb.text for cb in ElementFilter(kind=ui.checkbox, marker='Variable') if cb.value]
+                selected_levels = [cb._markers[1] for cb in ElementFilter(kind=ui.checkbox, marker='Level') if cb.value]
+
+                download_thread = (threading.Thread(
+                    target=lambda: GFS_download(hours=selected_hours, run_date=run_date.replace('gfs.', ''),
+                                               run_time=run_time,
+                                               filename=file,
+                                               levels=selected_levels, variables=selected_vars, subregion=subregion)
+                                              ,daemon=True))
+
+                download_thread.start()
+                download_thread.join()
+
+                dialog.clear()
+                with dialog, ui.card().style('padding: 20px;'):
+                    ui.label("Panoply Detected, Would you like to open it?")
+                    panoply_path = os.getenv("PANOPLY_PATH")
+                    grib_file = os.path.abspath(file)
+
+                    ui.button("Open With Panoply", on_click=lambda: (
+                    dialog.close(), subprocess.run(f'"{panoply_path}" "{grib_file}"', shell=True)))
+                    ui.button("Exit", on_click=dialog.close)
+            else:
+                ui.notify('Invalid range! "To" hour must be greater than "From" hour.', type='negative')
+
+        with (ui.dialog() as dialog, ui.card().style('padding: 20px;')):
+            ui.label('Select Forecast Hours Range').style('font-size: 16px; font-weight: bold;')
+
+            from_hour = ui.select(label='From Hour', options=hours)
+            to_hour = ui.select(label='To Hour', options=hours)
+
+            ui.button('Submit', on_click=lambda: (ui.notify("Download Has Finished"), confirm_selection())
+                      ).style('margin-top: 10px;')
+
+        if all([top_entry.value, left_entry.value, right_entry.value, bottom_entry.value]):
+            subregion = [int(float(top_entry.value)), int(float(bottom_entry.value)),
+                         int(float(left_entry.value)), int(float(right_entry.value))]
+        else:
+            subregion = None
+        dialog.open()
+    with ui.row():
+      run_dates = ui.select(options=list(nomads.get_available_runs().keys()))
+      runs = ui.select(options=[''])
+      run_dates.on_value_change(lambda : runs.set_options(nomads.get_available_runs().get(run_dates.value)))
+      generated_elements.extend([run_dates, runs])
+      download_button.on_click(lambda : Download(run_dates.value.strip('.gfs'), runs.value))
+
+
+
+def GEM_download(**kwargs):
+    urls=[]
+    hours = kwargs.get('hours')
+    levels=kwargs.get('levels')
+    variables= kwargs.get('variables')
+    run_time = kwargs.get('run_time')
+
+    for hour in hours:
+        for level, variable in product(levels, variables):
+            urls.append(GEM.create_url(hour, run_time, variable, 'ISBL', level))
+    download_object = Downloader(urls)
+    downloaded_data = download_object.submitdownloads()
+    GribCreation(downloaded_data, 'gem.grib').merge_grib_files()
+
+
+
+def GEM_Load():
+    raise NotImplementedError
+
