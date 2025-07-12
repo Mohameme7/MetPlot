0a28b4a8c09fd82803d1063acb9e3e44c3090138
Mohameme7 <donttakemeplease@gmail.com>
2025-07-12 19:24:16 +0400

    Added Progressbar Now It actually checks for panoply after downloading

--- GUI/modeldownloads.py
+++ GUI/modeldownloads.py
@@ -3,30 +3,41 @@ import subprocess
 import threading
 
 import webview
-from nicegui import ui, ElementFilter, app
+from nicegui import ui, ElementFilter, app, run, observables
+
 
 from MetPlot.Downloader.FileHandler import GribCreation
 from MetPlot.Downloader.MemoryDownload import Downloader
 from MetPlot.Downloader.Parsers.NomadsUtils import NomadsParse
 from MetPlot.Downloader.Parsers.GEM import GEM
 from itertools import product
+from multiprocessing import Manager
+import threading
+
 
 def GFS_download(**kwargs):
     hours = kwargs.get('hours')
     run_date = kwargs.get('run_date')
     run_time = kwargs.get('run_time')
     variables = kwargs.get('variables')
-    levels=kwargs.get('levels')
+    levels = kwargs.get('levels')
     filename = kwargs.get('filename')
     subregion = kwargs.get('subregion')
+    queue = kwargs.get('queue')
+
 
     urls = []
     for hour in hours:
+        urls.append(NomadsParse.create_url(hour, run_date, run_time, variables, levels, subregion))
 
-     urls.append(NomadsParse.create_url(hour, run_date, run_time, variables, levels, subregion))
-    download_object = Downloader(urls)
+
+    download_object = Downloader(urls, queue=queue if queue else None)
+
     downloaded_data = download_object.submitdownloads()
     GribCreation(downloaded_data, filename).merge_grib_files()
+
+
+
 
 def GFS_Load(download_button, top_entry, bottom_entry, left_entry, right_entry, generated_elements, content):
     nomads = NomadsParse(content)
@@ -42,31 +53,56 @@ def GFS_Load(download_button, top_entry, bottom_entry, left_entry, right_entry, 
         else:
             hours = list(range(1, 121, 1)) + list(range(123, 385, 3))
 
-        def confirm_selection():
+        async def confirm_selection():
             if int(to_hour.value) > int(from_hour.value):
                 selected_hours = list(range(int(from_hour.value), int(to_hour.value) + 1))
                 selected_vars = [cb.text for cb in ElementFilter(kind=ui.checkbox, marker='Variable') if cb.value]
                 selected_levels = [cb._markers[1] for cb in ElementFilter(kind=ui.checkbox, marker='Level') if cb.value]
+                queue = Manager().Queue()
 
-                download_thread = (threading.Thread(
-                    target=lambda: GFS_download(hours=selected_hours, run_date=run_date.replace('gfs.', ''),
-                                               run_time=run_time,
-                                               filename=file,
-                                               levels=selected_levels, variables=selected_vars, subregion=subregion)
-                                              ,daemon=True))
+                count = 0
 
-                download_thread.start()
-                download_thread.join()
+                count_lock = threading.Lock()
+
+                def count_handler(q):
+                    nonlocal count
+                    while True:
+                            q.get()
+                            with count_lock:
+                                count += 1
+                                prgrbar.set_value(count/len(hours))
+                counter_thread = threading.Thread(target=count_handler, args=(queue,), daemon=True)
+                counter_thread.start()
+                prgrbar = ui.linear_progress(value=0)
+
+                await run.cpu_bound(
+                    GFS_download,
+                    hours=selected_hours,
+                    run_date=run_date.replace('gfs.', ''),
+                    run_time=run_time,
+                    filename=file,
+                    levels=selected_levels,
+                    variables=selected_vars,
+                    subregion=subregion,
+                    queue=queue,
+                )
+
+
 
                 dialog.clear()
                 with dialog, ui.card().style('padding: 20px;'):
-                    ui.label("Panoply Detected, Would you like to open it?")
-                    panoply_path = os.getenv("PANOPLY_PATH")
-                    grib_file = os.path.abspath(file)
+                    ui.label("Download has finished.")
+                    if not os.getenv("PANOPLY_PATH"):
 
-                    ui.button("Open With Panoply", on_click=lambda: (
-                    dialog.close(), subprocess.run(f'"{panoply_path}" "{grib_file}"', shell=True)))
-                    ui.button("Exit", on_click=dialog.close)
+                        ui.button("Exit", on_click=dialog.close)
+                    else:
+                      ui.label("Panoply Detected, Would you like to open it?")
+
+                      grib_file = os.path.abspath(file)
+
+                      ui.button("Open With Panoply", on_click=lambda: (
+                        dialog.close(), subprocess.run(f'"{os.getenv("PANOPLY_PATH")}" "{grib_file}"', shell=True)))
+                      ui.button("Exit", on_click=dialog.close)
             else:
                 ui.notify('Invalid range! "To" hour must be greater than "From" hour.', type='negative')
 
@@ -76,7 +112,7 @@ def GFS_Load(download_button, top_entry, bottom_entry, left_entry, right_entry, 
             from_hour = ui.select(label='From Hour', options=hours)
             to_hour = ui.select(label='To Hour', options=hours)
 
-            ui.button('Submit', on_click=lambda: (ui.notify("Download Has Finished"), confirm_selection())
+            ui.button('Submit', on_click=confirm_selection
                       ).style('margin-top: 10px;')
 
         if all([top_entry.value, left_entry.value, right_entry.value, bottom_entry.value]):
@@ -85,22 +121,21 @@ def GFS_Load(download_button, top_entry, bottom_entry, left_entry, right_entry, 
         else:
             subregion = None
         dialog.open()
-    with ui.row():
-      run_dates = ui.select(options=list(nomads.get_available_runs().keys()))
-      runs = ui.select(options=[''])
-      run_dates.on_value_change(lambda : runs.set_options(nomads.get_available_runs().get(run_dates.value)))
-      generated_elements.extend([run_dates, runs])
-      download_button.on_click(lambda : Download(run_dates.value.strip('.gfs'), runs.value))
 
+    with ui.row():
+        run_dates = ui.select(options=list(nomads.get_available_runs().keys()))
+        runs = ui.select(options=[''])
+        run_dates.on_value_change(lambda: runs.set_options(nomads.get_available_runs().get(run_dates.value)))
+        generated_elements.extend([run_dates, runs])
+        download_button.on_click(lambda: Download(run_dates.value.strip('.gfs'), runs.value))
 
 
 def GEM_download(**kwargs):
-    urls=[]
+    urls = []
     hours = kwargs.get('hours')
-    levels=kwargs.get('levels')
-    variables= kwargs.get('variables')
+    levels = kwargs.get('levels')
+    variables = kwargs.get('variables')
     run_time = kwargs.get('run_time')
-
     for hour in hours:
         for level, variable in product(levels, variables):
             urls.append(GEM.create_url(hour, run_time, variable, 'ISBL', level))
@@ -109,7 +144,13 @@ def GEM_download(**kwargs):
     GribCreation(downloaded_data, 'gem.grib').merge_grib_files()
 
 
-
-def GEM_Load():
+def GEM_Load(download_button, generated_elements):
     raise NotImplementedError
-
+    ###
+    GEM_Parse = GEM()
+    with ui.row():
+        run_dates = ui.select(options=list(GEM_Parse.get_runs_hours().keys()))
+        runs = ui.select(options=[''])
+        run_dates.on_value_change(lambda: runs.set_options(GEM_Parse.get_runs_hours().get(run_dates.value)))
+        generated_elements.extend([run_dates, runs])
+        download_button.on_click(lambda: ui.notify("Test"))
