6345228d160b70e8b7074a1c2eca4fd85c2ce070
Mohameme7 <donttakemeplease@gmail.com>
2025-07-31 19:47:23 +0400

    Added External figure compatibility for PlotData for colormap testing

--- MetPlot/utils/CMAPTest.py
+++ MetPlot/utils/CMAPTest.py
@@ -23,6 +23,7 @@ class PlotInfo:
     smoothness: int = 5
 
 
+
 class PlotData(PlotInfo, MapDataGenerator):
     """Plots The Random data, probably going to be reduced to a more abstract version as a lot of stuff here will be
     used too much.
@@ -31,7 +32,7 @@ class PlotData(PlotInfo, MapDataGenerator):
     :param dots_per_inch : The resolution of the map, set to 300 by default Which is pretty good
     :param save_pic : Option to save a picture of a plot, should be a tuple, eg: (True, 'Cool_warmPlot.png')
     :param smoothness : The smoothness of data, higher numbers will output less variance in data
-
+    :param fig : Optional Figure of yours
     """
 
     def __init__(self, cmap: Union[str, io.TextIOWrapper, mcolors.LinearSegmentedColormap, mcolors.Colormap,
@@ -43,14 +44,20 @@ class PlotData(PlotInfo, MapDataGenerator):
                  dots_per_inch: int = 300,
                  save_pic: tuple = (False,),
                  smoothness: int = 5,
+                 fig : plt.Figure = None
+
                  ):
         super().__init__(cmap=cmap, toplat=toplat, rightlon=rightlon, leftlon=leftlon,
                          botlat=botlat, dots_per_inch=dots_per_inch, save_pic=save_pic, smoothness=smoothness)
         MapDataGenerator.__init__(self, toplat=toplat, rightlon=rightlon, leftlon=leftlon,
                                   botlat=botlat, smoothness=smoothness)
-        self.fig, self.ax = plt.subplots(figsize=(12, 8 ), subplot_kw={'projection': ccrs.PlateCarree()},
-                                         dpi=dots_per_inch)
-
+        self._external_fig = fig is not None
+        if not fig:
+          self.fig, self.ax = plt.subplots(figsize=(12, 8 ), subplot_kw={'projection': ccrs.PlateCarree()},
+                                       dpi=dots_per_inch)
+        else:
+            self.fig = fig
+            self.ax = fig.axes[0] if fig.axes else fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
         self.cmap = color_map_validator(self.cmap)
         self._plot()
 
@@ -64,11 +71,12 @@ class PlotData(PlotInfo, MapDataGenerator):
         contour = self.ax.contourf(self.griddedCoords[0], self.griddedCoords[1], self.data, cmap=self.cmap,
                                    transform=ccrs.PlateCarree())
 
-        plt.colorbar(contour, ax=self.ax, orientation='vertical')
+        self.fig.colorbar(contour, ax=self.ax, orientation='vertical')
         self.ax.set_title('Color Map Test')
 
         (plt.savefig(self.save_pic[1] if len(self.save_pic) > 1 else "plot.png", bbox_inches='tight')
          if self.save_pic[0] else None)
-        plt.show()
+        if not self._external_fig:
+            plt.show()
 
 
